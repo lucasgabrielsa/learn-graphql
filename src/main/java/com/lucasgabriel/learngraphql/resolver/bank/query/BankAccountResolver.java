7a0c433df000a49363f00b6429a044e26425ffff
Lucas Gabriel <lucasgabrielsa@hotmail.com>
2021-01-16 12:42:39 -0300

    DataFetchingEnvironment tests for use

--- src/main/java/com/lucasgabriel/learngraphql/resolver/bank/query/BankAccountResolver.java
+++ src/main/java/com/lucasgabriel/learngraphql/resolver/bank/query/BankAccountResolver.java
@@ -3,6 +3,8 @@ package com.lucasgabriel.learngraphql.resolver.bank.query;
 import com.lucasgabriel.learngraphql.domain.bank.BankAccount;
 import com.lucasgabriel.learngraphql.domain.bank.Currency;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.schema.SelectedField;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,10 @@ import java.util.UUID;
 @Component
 public class BankAccountResolver implements GraphQLQueryResolver {
 
-    public BankAccount bankAccount(UUID id) {
+    public BankAccount bankAccount(UUID id, DataFetchingEnvironment e) {
       log.info("Retrieving bank account id {}", id);
+
+      e.getSelectionSet().getFields().stream().map(SelectedField::getName).forEach(System.out::println);
 
       return BankAccount.builder().id(id).currency(Currency.USD)
               .build();
